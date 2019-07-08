263443c9924fd620fac0938f11ee6200911d5d94
League Student <league@iMac.attlocal.net>
2019-07-07 17:32:07 -0700

    asdf

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -21,6 +21,48 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
+		
+
+		int robots = Integer.parseInt(JOptionPane.showInputDialog("how many robots?"));
+		
+		String colorStr = JOptionPane.showInputDialog("what color? (red, green, or blue)");
+		Color color;
+		if (colorStr.toLowerCase().trim().equals("red")) {
+			color = Color.red;
+		} else if (colorStr.toLowerCase().trim().equals("green")) {
+			color = Color.green;
+		} else if (colorStr.toLowerCase().trim().equals("blue")) {
+			color = Color.blue;
+		} else {
+			JOptionPane.showMessageDialog(null, "No. It's gonna be black");
+			color = Color.black;
+		}
+		
+		int sides = Integer.parseInt(JOptionPane.showInputDialog("how many sides?"));
+		
+		Thread[] threads = new Thread[robots];
+		
+		int turn = 360/sides;
+		double drive = 100 * Math.sin((turn/2)*(Math.PI/180));
+		
+		for (int i = 0; i < robots; i++) {
+			int num = i;
+			threads[i] = new Thread(() -> {
+				Robot robot = new Robot((num + 1) * 150, 300);
+				robot.setSpeed(100);
+				robot.penDown();
+				robot.setPenColor(color);
+				for (int j = 0; j < sides; j++) {
+					robot.move((int)drive);
+					robot.turn(turn);
+				}
+				
+			});
+		}
+		
+		for (int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
 
 	}
 }
